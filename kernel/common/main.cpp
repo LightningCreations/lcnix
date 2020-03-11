959f858e5c4163d8cc362ac9a0dea9ec7b7df9af
Connor Horman <chorman64@gmail.com>
2020-03-11 19:18:11 -0400

    Basic stuff

---
+++ kernel/common/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar. 11, 2020
+ *      Author: connor
+ */
+
+
+
+
---
+++ kernel/dummy.cpp
@@ -0,0 +1,10 @@
+/*
+ * dummy.cpp
+ *
+ *  Created on: Mar. 11, 2020
+ *      Author: Connor Horman
+ */
+
+
+
+
