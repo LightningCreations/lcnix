bf6e2a8e536f888103651e397b7ee27f5f54dd5c
Connor Horman <chorman64@gmail.com>
2020-03-12 18:45:30 -0400

    wrmsrs

--- kernel/arch/x86_64/syscall_init.s
+++ kernel/arch/x86_64/syscall_init.s
@@ -1,4 +1,18 @@
 
 .text
 
+.syscall_handler:
+    call __syscall
+    sysret
+
+
 init_syscall_handle:
+    lea .syscall_handler,rax
+    mov rax,rdx
+    shr rdx,$32
+    mov $0x82,ecx
+    wrmsr
+    mov $0x83,ecx
+    wrmsr
+    # set both LSTAR and CSTAR
+
