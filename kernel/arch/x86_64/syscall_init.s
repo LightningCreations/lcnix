3b8d8883b24cc5f2d835aed85025cfac54eea1bf
Connor Horman <chorman64@gmail.com>
2020-03-19 19:39:12 -0400

    Fix assembly
    I definately did not leave some Intel Syntax in my AT&T Code.
    Nope, not at all

--- kernel/arch/x86_64/syscall_init.s
+++ kernel/arch/x86_64/syscall_init.s
@@ -7,12 +7,12 @@
 
 
 init_syscall_handle:
-    lea .syscall_handler,rax
-    mov rax,rdx
-    shr rdx,$32
-    mov $0x82,ecx
+    lea .syscall_handler,%rax
+    mov %rax,%rdx
+    shr %rdx,$32
+    movd $0x82,%ecx
     wrmsr
-    mov $0x83,ecx
+    movd $0x83,%ecx
     wrmsr
     # set both LSTAR and CSTAR
 
